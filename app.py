7f05d28b241efba0b3a6a70ce3dd083c47993120
vadim <pmib-192_535838@volsu.ru>
2024-10-23 13:03:58 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import json
+from PIL import Image
+import numpy as np
+from train_test import rec_digit
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/NumberRecognition", methods=["POST"])
+def predict_digit():
+    pil_img = Image.open(request.files["img"]).convert("L")
+    img = np.array(pil_img)
+
+    # predict
+    res_json = {"pred": 0, "probs": [0]}
+    res = rec_digit(img)
+    res_json["pred"] = int(res.argmax())
+    res_json["probs"] = (res * 100).tolist()
+
+    return json.dumps(res_json)
---
+++ train_test.py
@@ -0,0 +1,107 @@
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import keras
+import json
+import math
+from scipy.ndimage import center_of_mass
+
+# тренировка новой модели
+train_new_model = False
+
+if train_new_model:
+    # загрузка MNIST и разбиение на тестовые и тренировочные данные
+    mnist = tf.keras.datasets.mnist
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+    # нормирование данных
+    X_train = tf.keras.utils.normalize(X_train, axis=1)
+    X_test = tf.keras.utils.normalize(X_test, axis=1)
+
+    # создание модели
+    # плоский входной слой для пикселей
+    # два скрытых полносвязных слоя
+    # один односвязный выходной слой для 10 чисел
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+    model.add(tf.keras.layers.Dense(units=128, activation='relu'))
+    model.add(tf.keras.layers.Dense(units=10, activation='softmax'))
+
+    # компиляция и оптимизация модели
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+    # тренировка модели
+    model.fit(X_train, y_train, epochs=3)
+    # оценка модели
+    val_loss, val_acc = model.evaluate(X_test, y_test)
+    print(val_loss)
+    print(val_acc)
+
+    # сохранение
+    model.save("checkpoint/handwritten_digits.keras")
+else:
+    model = keras.saving.load_model("checkpoint/handwritten_digits.keras")
+print('yay')
+
+def getBestShift(img):
+    cy, cx = center_of_mass(img)
+
+    rows, cols = img.shape
+    shiftx = np.round(cols / 2.0 - cx).astype(int)
+    shifty = np.round(rows / 2.0 - cy).astype(int)
+
+    return shiftx, shifty
+
+
+def shift(img, sx, sy):
+    rows, cols = img.shape
+    M = np.float32([[1, 0, sx], [0, 1, sy]])
+    shifted = cv2.warpAffine(img, M, (cols, rows))
+    return shifted
+
+
+def rec_digit(img):
+    #img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    gray = 255 - img
+    # применяем пороговую обработку
+    (thresh, gray) = cv2.threshold(gray, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    # удаляем нулевые строки и столбцы
+    while np.sum(gray[0]) == 0:
+        gray = gray[1:]
+    while np.sum(gray[:, 0]) == 0:
+        gray = np.delete(gray, 0, 1)
+    while np.sum(gray[-1]) == 0:
+        gray = gray[:-1]
+    while np.sum(gray[:, -1]) == 0:
+        gray = np.delete(gray, -1, 1)
+    rows, cols = gray.shape
+
+    # изменяем размер, чтобы помещалось в box 20x20 пикселей
+    if rows > cols:
+        factor = 20.0 / rows
+        rows = 20
+        cols = int(round(cols * factor))
+        gray = cv2.resize(gray, (cols, rows))
+    else:
+        factor = 20.0 / cols
+        cols = 20
+        rows = int(round(rows * factor))
+        gray = cv2.resize(gray, (cols, rows))
+
+    # расширяем до размера 28x28
+    colsPadding = (int(math.ceil((28 - cols) / 2.0)), int(math.floor((28 - cols) / 2.0)))
+    rowsPadding = (int(math.ceil((28 - rows) / 2.0)), int(math.floor((28 - rows) / 2.0)))
+    gray = np.lib.pad(gray, (rowsPadding, colsPadding), 'constant')
+
+    # сдвигаем центр масс
+    shiftx, shifty = getBestShift(gray)
+    shifted = shift(gray, shiftx, shifty)
+    gray = shifted
+
+    #cv2.imwrite('test numbers/digit{}.png'.format(img_number), gray)
+    img = gray / 255.0
+    img = np.array(img).reshape(-1, 28, 28, 1)
+    return model.predict(img)
